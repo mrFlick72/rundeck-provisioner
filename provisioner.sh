f06c5b7253e191d8e558f4123da600732ebfa2da
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-22 14:49:41 +0100

    removed useless sudo command

--- provisioner.sh
+++ provisioner.sh
@@ -1,20 +1,20 @@
-sudo setenforce 0
-sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-sudo modprobe br_netfilter
+setenforce 0
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+modprobe br_netfilter
 
-sudo echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
-sudo echo "net.bridge.bridge-nf-call-ip6tables=1" | sudo tee -a /etc/sysctl.conf
-sudo echo "net.bridge.bridge-nf-call-arptables=1" | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-ip6tables=1" | sudo tee -a /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-arptables=1" | sudo tee -a /etc/sysctl.conf
+sysctl -p
 
-sudo yum update -y 
-sudo yum install -y vim 
+yum update -y
+yum install -y vim
 
 rpm -Uvh http://repo.rundeck.org/latest.rpm
-sudo yum install -y rundeck java
+yum install -y rundeck java
 
-sudo yum install -y epel-release
-sudo yum install -y ansible
+yum install -y epel-release
+yum install -y ansible
 
 cd /etc/rundeck
 
@@ -22,4 +22,4 @@ RUNDECK_PORT="8080"
 sed -i "s/localhost:4440/10.200.0.10:$RUNDECK_PORT/g" rundeck-config.properties 
 echo "server.port=${RUNDECK_PORT}" >> rundeck-config.properties 
 
-sudo service rundeckd start
+service rundeckd start
